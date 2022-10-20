d75400a95cc6333771597cee3ccf962e5a277c60
Sulaymane Sillah <sssillah1@gmail.com>
2022-10-20 18:34:22 +0900

    added regex

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,16 +3,30 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
-let vowels = ['a','e','i','o','u']
+// let vowels = ['a','e','i','o','u']
 
-function vowelsCounter(text) {
-    // Code goes here
-    let counter = 0
-    for (let letter of text.toLowerCase()){
-        if(vowels.includes(letter)){
-            counter++
-        }
-    } return counter
+// function vowelsCounter(text) {
+//     // Code goes here
+//     let counter = 0
+//     for (let letter of text.toLowerCase()){
+//         if(vowels.includes(letter)){
+//             counter++
+//         }
+//     } return counter
+// }
+
+
+//regex approach
+function vowelsCounter(text){
+    //search text with regex and store all matching instances
+    let matchinginstances = text.match(/[aeiou]/gi)
+
+    //check if matching instances exists and if so, calculate length
+    if(matchinginstances){
+        return matchinginstances.length
+    }else{
+        return 0
+    }
 }
 
 console.log(vowelsCounter("antidisestablishmentarianism"))
