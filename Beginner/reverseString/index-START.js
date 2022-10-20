cab39878bcc3883cf12a9d83b33b94951d45d8d9
Sulaymane Sillah <sssillah1@gmail.com>
2022-10-20 17:58:38 +0900

    finished vowel counter

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -5,10 +5,22 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 
-function reverseString(text) {
-    // Code goes here
+// function reverseString(text) {
+//     // Code goes here
+//     return [...text].reverse().join('')
+// }
+
+// function reverseString(text){
+//     let result = ''
+
+//     for (let i = text.length-1; i >= 0; i--){
+//         result += text[i]
+//     } 
+//     return result
+// }
+
+function reverseString(text){
+    return [...text].reduce((acc,char) => char + acc, '')
 }
-
-
 
 module.exports = reverseString
--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,10 +3,19 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+let vowels = ['a','e','i','o','u']
 
 function vowelsCounter(text) {
     // Code goes here
+    let counter = 0
+    for (let letter of text.toLowerCase()){
+        if(vowels.includes(letter)){
+            counter++
+        }
+    } return counter
 }
+
+console.log(vowelsCounter("antidisestablishmentarianism"))
 
 
 
