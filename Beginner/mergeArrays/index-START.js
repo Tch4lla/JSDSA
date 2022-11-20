01dddd20b4ba509dd4abc61966cd00506dec710a
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-21 07:21:57 +0900

    updated array

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,10 +4,18 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
+//spread all of the arrays in the parameters so that it is one single array
 function mergeArrays(...arrays) {
+    //initialize an empty array to store all of the array values 
+    let jointArray = []
 
-    
-    
+    //loop through all of the arrays
+    //for each array, reassign jointArray to include all the joint array values and the array values 
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    })
+    //return a new set from the joint array values, eliminating all duplicates
+    return [...new Set([...jointArray])]
 }
 
 
