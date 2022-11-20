c585f3d1ee44f6c1e9b8068a4a0855eb3bd46454
Sulaymane Sillah <sssillah1@gmail.com>
2022-11-21 07:09:09 +0900

    updates

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -4,9 +4,31 @@
  E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function chunkArray(array, size) {
-    // Code goes here
-}
+// function chunkArray(array, size) {
+//     let result = []
+//     for (value of array){
+//         let lastArray = result[result.length -1 ]
+        
+//         if(!lastArray || lastArray.length == size){
+//             result.push([value])
+//         } else{
+//             lastArray.push(value)
+//         }
+//     }
+//     return result
+// }
 
+function chunkArray(array,size){
+    let copy = array
+    let result = []
+   
+
+    while(copy.length > 0){
+        result.push(copy.splice(0,size))
+    }
+    return result
+
+}
+console.log(chunkArray([1,2,3,4,5,6,7,8],2))
 
 module.exports = chunkArray
--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -6,14 +6,7 @@
 
 function mergeArrays(...arrays) {
 
-    let jointArray = []
     
-    arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
-
-    return [...new Set([...jointArray])]
-
     
 }
 
